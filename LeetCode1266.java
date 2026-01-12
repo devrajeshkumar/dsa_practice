e51e4a650f36b5f6169423e109aa256de70e944d
Rajesh Kumar <rajeshkumarfzd101199@gmail.com>
2026-01-12 12:14:02 +0530

    practice 12 january 2026

---
+++ LeetCode1266.java
@@ -0,0 +1,9 @@
+public class LeetCode1266 {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int min = 0;
+        for (int i = 0; i < points.length - 1; i++) {
+            min += Math.max(Math.abs(points[i][0] - points[i + 1][0]), Math.abs(points[i][1] - points[i + 1][1]));
+        }
+        return min;
+    }
+}
---
+++ LeetCode865.java
@@ -0,0 +1,42 @@
+import java.util.ArrayDeque;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class LeetCode865 {
+
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        ArrayDeque<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        TreeNode result = null;
+        while (!q.isEmpty()) {
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = q.pollFirst();
+                if (i == 0)
+                    result = node;
+                if (node.left != null)
+                    q.addLast(node.left);
+                if (node.right != null)
+                    q.addLast(node.right);
+            }
+        }
+        return result;
+    }
+}
