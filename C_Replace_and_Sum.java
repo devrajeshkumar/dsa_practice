cb584e4bf555848530db08235f5fc57d35935966
Rajesh Kumar <rajeshkumarfzd101199@gmail.com>
2026-01-30 21:53:06 +0530

    practice 30 jan 2026

---
+++ C_Replace_and_Sum.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class C_Replace_and_Sum {
+
+    public static void main(String[] args) {
+        solve();
+    }
+
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt(), q = sc.nextInt();
+            int a[] = new int[n];
+            int b[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            for (int i = 0; i < n; i++) {
+                b[i] = sc.nextInt();
+            }
+            for (int i = 0; i < n; i++) {
+                if (i < n - 1) {
+                    a[i] = Math.max(a[i], Math.max(a[i + 1], b[i]));
+                } else {
+                    a[i] = Math.min(a[i], b[i]);
+                }
+            }
+
+            int prefixSum[] = new int[n + 1];
+            prefixSum[1] = a[0];
+            for (int i = 1; i < n; i++) {
+                prefixSum[i + 1] = prefixSum[i] + a[i];
+            }
+            while (q-- > 0) {
+                int l = sc.nextInt(), r = sc.nextInt();
+                System.out.print(prefixSum[r] - prefixSum[l - 1] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
