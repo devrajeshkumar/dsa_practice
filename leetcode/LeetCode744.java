2dbf0f6d643cf0dce44592feb5089b8fe4477cea
Rajesh Kumar <rajeshkumarfzd101199@gmail.com>
2026-01-31 20:24:06 +0530

    practice 31 jan 2026

---
+++ leetcode/LeetCode744.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class LeetCode744 {
+    public char nextGreatestLetter(char[] letters, char target) {
+        char result = letters[0];
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] > target) {
+                result = letters[i];
+                break;
+            }
+        }
+        return result;
+    }
+}
